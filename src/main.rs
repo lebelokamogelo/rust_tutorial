61edbdcd69fe85aba02080fba62ad3b231c640da
lebelokamogelo <lebelokamogelo47@gmail.com>
2024-03-05 21:10:55 +0200

    read input from the console

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::io::stdin;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Please enter your name ");
+
+    let mut name = String::new();
+
+    stdin() // stdin function allow us to handle user input
+    .read_line(&mut name)
+    .expect("Failed to read line");
+
+    print!("Your name is {}", name);
 }
