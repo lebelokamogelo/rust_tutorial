3435bbfbe8e8c8206f809657ada325f3a1b44764
lebelokamogelo <lebelokamogelo47@gmail.com>
2024-04-21 22:59:47 +0200

    reading a file

--- src/module/args.rs
+++ src/module/args.rs
@@ -1,7 +1,12 @@
 use std::env::args;
+use std::fs;
 
 pub fn env_args() {
     let args: Vec<String> = args().collect();
-    let arg = &args[1];
-    println!("{}", arg);
+    let file_path = &args[1];
+    // Reading a File
+    // fs crate to read the file
+    // read_to_string to read the content of the file
+    let content = fs::read_to_string(file_path).expect("Failed to read the file");
+    println!("{}", content);
 }
