21564126de34e7507803395bb29a11c34f133943
lebelokamogelo <lebelokamogelo47@gmail.com>
2024-04-30 21:00:27 +0200

    smart pointer with Rc

--- src/module/smart_pointers.rs
+++ src/module/smart_pointers.rs
@@ -18,6 +18,7 @@
 //
 //  Defining Our Own Smart Pointer
 use std::ops::Deref;
+use std::rc::Rc;
 
 #[derive(Debug)]
 struct MyBox<T>(T);
@@ -47,6 +48,20 @@ impl Drop for CustomSmartPointer {
     }
 }
 
+// Rc<T>, the Reference Counted Smart pointer
+//  The Rc<T> type keeps track of the number of references to a value to determin
+//  whether or not the value is still in use. If there are zero references to a value,
+//  the value can be cleaned up without any references becoming invalid.
+//
+/*
+Imagine Rc<T> as a TV in a family room. When one person enters to watch TV, they turn it on. Others can come into the room and watch the TV. When the last person leaves the room, they turn off the TV because it’s no longer being used. If someone turns off the TV while others are still watching it, there would be uproar from the remaining TV watchers!
+*/
+// Define a struct representing the TV program
+#[derive(Debug)]
+struct TvProgram {
+    name: String,
+}
+
 pub fn smart_pointer() {
     //let b = Box::new(5);
     // println!("b = {}", b);
@@ -65,4 +80,36 @@ pub fn smart_pointer() {
     drop(c);
     // Value dropped : below code is compiler error trying to access the value moved
     //println!("CustomSmartPointer dropped before the end of main. {}", c.data);
+    //
+    // Create a shared TV program
+    let tv_program = Rc::new(TvProgram {
+        name: String::from("Rust Programming"),
+    });
+
+    // Person 1 enters the room to watch TV
+    let person1 = Rc::clone(&tv_program);
+    println!("Person 1 enters the room to watch TV: {:?}", person1);
+
+    // Person 2 enters the room to watch TV
+    let person2 = Rc::clone(&tv_program);
+    println!("Person 2 enters the room to watch TV: {:?}", person2);
+
+    // Person 1 leaves the room
+    drop(person1);
+    println!("Person 1 leaves the room");
+
+    // Person 2 leaves the room
+
+    println!("Person 2 leaves the room");
+
+    // Now, there are no more references to the TV program, it's deallocated
+
+    println!("TV program: {:?}", tv_program);
+    println!("Count is {}", Rc::strong_count(&tv_program));
+    drop(tv_program);
+
+    println!(
+        "Person2 still have access to it until it is dropped: {:?}",
+        person2
+    );
 }
