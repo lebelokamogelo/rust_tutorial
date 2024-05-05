a451a0a75563b0f8ed1a10319e0d8a79cee77524
lebelokamogelo <lebelokamogelo47@gmail.com>
2024-05-05 08:44:49 +0200

    message passing using mpsc single message

--- src/module/thread.rs
+++ src/module/thread.rs
@@ -5,12 +5,16 @@ use std::{sync::mpsc, thread, time::Duration};
 //
 pub fn thread() {
     let (tx, rx) = mpsc::channel();
+    // mpsc stands for multiple producer, single consumer
     // spawn to fire up a new thread
+    //
+    // using move to move tx into the closure so the spawned thread owns tx
     let thread1 = thread::spawn(move || {
         // Using Message Passing to Transfer Data Between Threads
         // To accomplish message-sending concurrency, Rust's standard library provides an
         // implementation of channels. A channel is a general programming concept by
         // which data is sent from one thread to another.
+        // A channel has two halves: a transmitter and a receiver.
 
         let val = String::from("hi");
         tx.send(val).unwrap();
@@ -38,6 +42,13 @@ pub fn thread() {
 
     // Waiting for All Threads to Finish Using join Handles
     thread1.join().unwrap();
+
+    // The receiver has two useful methods: recv and try_recv
+    // We’re using recv, short for receive, which will block the main thread’s execution
+    // and wait until a value is sent down the channel.
+    //
+    // try_recv is useful if this thread has other work to do while waiting for messages
+    println!("Hello world");
 
     let received = rx.recv().unwrap();
     println!("Got: {}", received);
