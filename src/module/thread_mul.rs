45e6d1bc0e8902c2604c9e74486c3f9f0733fe2a
lebelokamogelo <lebelokamogelo47@gmail.com>
2024-05-05 09:39:41 +0200

    multiple message passing

--- src/module/thread_mul.rs
+++ src/module/thread_mul.rs
@@ -5,6 +5,13 @@ pub fn thread_mul() {
 
     let (tx, rx) = mpsc::channel();
 
+    let tx1 = tx.clone();
+    // This time, before we create the first spawned thread, we call clone on
+    // the transmitter. This will give us a new transmitter we can pass to the
+    // first spawned thread. We pass the original transmitter to a second spawned
+    // thread. This gives us two threads, each sending different messages to
+    // the one receiver.
+
     thread::spawn(move || {
         let vals = vec![
             String::from("hi"),
@@ -19,7 +26,25 @@ pub fn thread_mul() {
         }
     });
 
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("more"),
+            String::from("messages"),
+            String::from("for"),
+            String::from("you"),
+        ];
+
+        for val in vals {
+            tx1.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+
     for received in rx {
         println!("Got: {}", received);
     }
+    // Creating Multiple Producers by Cloning the Transmitter
+    //
+    // create multiple threads that all send values to the same receiver.
+    // We can do so by cloning the transmitter
 }
