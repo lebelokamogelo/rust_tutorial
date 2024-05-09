0d8896bd560c028ad2429c4e787913ca438f6dba
lebelokamogelo <lebelokamogelo47@gmail.com>
2024-05-09 22:03:07 +0200

    advance fn. fn pointer

--- src/module/fn_advance.rs
+++ src/module/fn_advance.rs
@@ -1,3 +1,15 @@
+fn add_one(x: i32) -> i32 {
+    x + 1
+}
+
+// The fn type is called a function pointer. Passing functions with
+// function pointers will allow you to use functions as arguments to other functions.
+fn do_twice(f: fn(i32) -> i32, arg: i32) -> i32 {
+    f(arg) + f(arg)
+}
+
 pub fn fn_advance() {
-    println!("Hello fn");
+    let answer = do_twice(add_one, 5);
+
+    println!("The answer is: {}", answer);
 }
